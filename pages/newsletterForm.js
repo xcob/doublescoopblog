d99bd8503ed709def615bb07425b87f8e178ce02
cob <34405172+xcob@users.noreply.github.com>
2023-04-25 22:02:01 -0500

    newsletter form scrollbar

--- pages/newsletterForm.js
+++ pages/newsletterForm.js
@@ -3,12 +3,12 @@ export default function NewsletterSignUp(){
         <div id="subscribe">
         <iframe 
             width="540" 
-            height="305" 
+            height="450" 
             src="https://53ce1a51.sibforms.com/serve/MUIEAEWmL9JyxugZYz_X1XWgxGyiFXDyvbX3QRAq0EUEJY-vdKINksNTkJahi_KFKMmN21A-zLTHTWIT_hPv0s5_iJbV3hXBvVGfbc7ITYRM5q31kvYZZMSMe9MIPWKVBhWxabW_-yqp2kydfPuVHqbgr_tpzbbUBKAyZhNe3ZswVEjTHXDCi2dOmnJYTfd9gXM5hItC6p2F9vDK" 
             frameborder="0" 
             scrolling="auto" 
             allowfullscreen 
-            style={{display: "flex",marginLeft:"auto", marginRight: "auto", maxWidth: "100%;", height: "350px"}}>
+            style={{display: "flex",marginLeft:"auto", marginRight: "auto", maxWidth: "100%;", height: "425px"}}>
 
         </iframe>
         </div>
